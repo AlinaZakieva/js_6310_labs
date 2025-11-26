4fec43808ca565633590c4dd1a2da27396f2b223
Alina Zakieva <jdfhkjd@email.ru>
2025-11-26 19:51:00 +0300

    Remove lab4 shoestore-bot from lab5 branch

--- lab4-node/solutions/student-13/shoestore-bot/src/utils/validation.js
+++
@@ -1,9 +0,0 @@
-export function parseNumber(value) {
-  if (typeof value !== "string") return null;
-
-  const normalized = value.replace(",", ".").trim();
-  const num = Number(normalized);
-
-  if (Number.isNaN(num)) return null;
-  return num;
-}
