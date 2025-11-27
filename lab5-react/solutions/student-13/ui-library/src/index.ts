c7227eb7cd3ff52eb1d7cac5fcdb09251a2cdff8
Alina Zakieva <jdfhkjd@email.ru>
2025-11-27 19:40:45 +0300

    Remove lab5 solution from lab6 branch

--- lab5-react/solutions/student-13/ui-library/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from './Button/Button'
-export type { ButtonProps } from './Button/Button'
-
-export { Card } from './Card/Card'
-export type { CardProps } from './Card/Card'
-
-export * from './ProductCard/ProductCard'
-export * from './ProductCatalog/ProductCatalog'
